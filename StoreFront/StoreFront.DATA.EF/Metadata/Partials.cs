f66fbe7d2ce1a35d1e78616931e7d4270842cf6e
nick sachau <nicksachau156@gmail.com>
2023-02-17 13:49:35 -0700

    restarting

---
+++ StoreFront/StoreFront.DATA.EF/Metadata/Partials.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreFront.DATA.EF.Models//Metadata
+{
+    internal class Partials
+    {
+        //Example:
+        //[ModelMetadataType(typeof(TypeMetadata))]
+        //public partial class TypeMetadata { }
+
+        //make one for City, Order, Pokemon, and Type
+    }
+}
